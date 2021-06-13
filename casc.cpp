23b985df1924673738663ede88c34e2a0a2530a3
lucas <85533116+tokisuno@users.noreply.github.com>
2021-06-13 11:02:44 -0400

    Add files via upload

---
+++ casc.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a,b;
+	
+	cout << "enter the value of a and b: " << endl;
+	cin >> a >> b;
+	
+	cout << "A: " << a << ", B: " << b << endl;
+
+	return 0;
+}
